fe1f6196ac5ed691fd4b366a97f93ab35dc16af8
andfsanchezag <andres.sanchez57@tdea.edu.co>
2026-08-18 16:33:04 -0500

    ejemplo de actualizacion

--- bank/src/main/java/application/BankApplication.java
+++ bank/src/main/java/application/BankApplication.java
@@ -2,7 +2,7 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//ajuste para commit ejemplo
+
 @SpringBootApplication
 public class BankApplication {
 
