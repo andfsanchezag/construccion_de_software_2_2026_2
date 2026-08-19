37f58c1f877b5901618c825e0102111f455e080d
andfsanchezag <andres.sanchez57@tdea.ed.co>
2026-08-19 16:39:31 -0500

    foto

--- bank/src/main/java/application/BankApplication.java
+++ bank/src/main/java/application/BankApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankApplication {
 
+	//esto es nuevo y no esta montado
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
